56dd5bc3dda8736fcd69d9cb8caa52302e4facad
HasanC14 <chowdhuryh999@gmail.com>
2023-10-15 23:48:27 +0600

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+import { Accelerometer } from "expo-sensors";
 
-export default function App() {
+const App = () => {
+  const [stepCount, setStepCount] = useState(0);
+
+  useEffect(() => {
+    const subscription = Accelerometer.addListener((accelerometerData) => {
+      const { x, y, z } = accelerometerData;
+      const deltaX = Math.abs(x);
+      const deltaY = Math.abs(y);
+      const deltaZ = Math.abs(z);
+
+      if (deltaX + deltaY + deltaZ > 3) {
+        setStepCount((prevCount) => prevCount + 1);
+      }
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 30 }}>Step Count: {stepCount}</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
