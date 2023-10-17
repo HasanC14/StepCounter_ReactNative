c2def609abda196c66a63215b1189d8c0169abf5
HasanC14 <chowdhuryh999@gmail.com>
2023-10-18 00:15:25 +0600

    set goal functionality added

--- App.js
+++ App.js
@@ -1,30 +1,93 @@
 import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import { View, Text, TextInput, Button } from "react-native";
 import { Accelerometer } from "expo-sensors";
+import Svg, { Circle, Text as SvgText } from "react-native-svg";
 
 const App = () => {
   const [stepCount, setStepCount] = useState(0);
+  const [goal, setGoal] = useState(null);
+  const [congratulate, setCongratulate] = useState(false);
 
   useEffect(() => {
-    const subscription = Accelerometer.addListener((accelerometerData) => {
-      const { x, y, z } = accelerometerData;
-      const deltaX = Math.abs(x);
-      const deltaY = Math.abs(y);
-      const deltaZ = Math.abs(z);
+    let subscription;
+    if (goal !== null) {
+      subscription = Accelerometer.addListener((accelerometerData) => {
+        const { x, y, z } = accelerometerData;
+        const deltaX = Math.abs(x);
+        const deltaY = Math.abs(y);
+        const deltaZ = Math.abs(z);
 
-      if (deltaX + deltaY + deltaZ > 3) {
-        setStepCount((prevCount) => prevCount + 1);
-      }
-    });
+        if (deltaX + deltaY + deltaZ > 3) {
+          setStepCount((prevCount) => {
+            if (prevCount + 1 === parseInt(goal, 10)) {
+              setCongratulate(true);
+            }
+            return prevCount + 1;
+          });
+        }
+      });
+    }
 
     return () => {
-      subscription.remove();
+      if (subscription) {
+        subscription.remove();
+      }
     };
-  }, []);
+  }, [goal]);
+
+  const handleSetGoal = () => {
+    if (goal && goal > 0) {
+      setStepCount(0);
+      setCongratulate(false);
+    }
+  };
+
+  const color = congratulate ? "green" : "red";
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 30 }}>Step Count: {stepCount}</Text>
+      {goal === null && (
+        <>
+          <TextInput
+            style={{
+              height: 40,
+              borderColor: "gray",
+              borderWidth: 1,
+              marginBottom: 20,
+              paddingHorizontal: 10,
+            }}
+            placeholder="Enter step goal"
+            keyboardType="numeric"
+            onEndEditing={(event) => setGoal(event.nativeEvent.text)}
+          />
+          <Button title="Set Goal" onPress={handleSetGoal} />
+        </>
+      )}
+      <Svg height="200" width="200">
+        <Circle
+          cx="100"
+          cy="100"
+          r="90"
+          stroke="black"
+          strokeWidth="2"
+          fill={color}
+        />
+        <SvgText
+          x="50%"
+          y="50%"
+          textAnchor="middle"
+          fontSize="40"
+          fill="white"
+          dy="10"
+        >
+          {stepCount}
+        </SvgText>
+      </Svg>
+      {congratulate && (
+        <Text style={{ fontSize: 20, marginTop: 10, color: "green" }}>
+          Congratulations! You've reached your goal of {goal} steps!
+        </Text>
+      )}
     </View>
   );
 };
